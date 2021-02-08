1525ff6e2931ff4e1083b1dd1544cdf3952c32a4
RemyLansade <boblol77@live.fr>
2021-02-08 22:52:04 +0100

    installing joi package to validate required fields

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const Joi = require('joi'); // Validate the data coming from /signup form fields.
 
 const router = express.Router();
+
+const schema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required()
+});
 
 router.get('/', (req, res) => {
     res.json({
         message: '🔐'
     });
+});
+
+router.post('/signup', (req, res) => {
+    const result = schema.validate(req.body);
+    res.json(result);
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const volleyball = require('volleyball'); // Volleyball is a tiny HTTP logger for debugging
 
@@ -6,33 +7,35 @@ const app = express();
 const auth = require('./auth/index');
 
 app.use(volleyball);
+app.use(express.json()); // Replace bodyParser.json()
+
+app.set('port', process.env.PORT || 5000);
 
 app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨Hello World! 🌈✨🦄'
-  });
+    res.json({
+        message: '🦄🌈✨Hello World! 🌈✨🦄'
+    });
 });
 
 app.use('/auth', auth);
 
 function notFound(req, res, next) {
-  res.status(404);
-  const error = new Error('Not Found - ' + req.originalUrl);
-  next(error);
+    res.status(404);
+    const error = new Error('Not Found - ' + req.originalUrl);
+    next(error);
 }
 
 function errorHandler(err, req, res, next) {
-  res.status(res.statusCode || 500);
-  res.json({
-    message: err.message,
-    stack: err.stack
-  });
+    res.status(res.statusCode || 500);
+    res.json({
+        message: err.message,
+        stack: err.stack
+    });
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Listening on port', port);
+app.listen(app.get('port'), () => {
+    console.log('Listening on port: ' + app.get('port'));
 });
